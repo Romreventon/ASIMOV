eb17b58e4a04afaf4aa4e913367ab600f7a55328
Romain Poupin <34235726+Romreventon@users.noreply.github.com>
2018-02-15 09:13:25 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
   <body>
     <?php
-    if (isset($_SESSION['prenom'])) {
+    if (isset($_SESSION['id'])) {
     ?>
       <div class="nav">
         <a href="#" class="home">Home</a>
@@ -24,48 +24,56 @@
     <?php
     } else {
     ?>
-  
+      <div class="header"><img src="Logo_ASIMOV.png" width="5%"></div>
+      <div class="nav">
+        <a href="#" class="home">Home</a>
+        <a href="#">Actualités</a>
+        <a href="#">Evénements</a>
+        <a href="#">Code_Space()</a>
+        <a href="#">Mon compte</a>
+        <a href="#" onclick="document.getElementById('id01').style.display='block'" style="width:auto";>Login</a>
+      </div>
 
-      <form action="#">
-        <div class="imgcontainer">
-          <img src="Logo_ASIMOV.png" alt="logo asimov" class="logo">
+        <div id="id01" class="modal">
+          
+          <form class="modal-content animate" action="/action_page.php">
+            <div class="imgcontainer">
+              <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+              <img src="img_avatar2.png" alt="Avatar" class="avatar">
+            </div>
+
+            <div class="container">
+              <label for="uname"><b>Username</b></label>
+              <input type="text" placeholder="Enter Username" name="uname" required>
+
+              <label for="psw"><b>Password</b></label>
+              <input type="password" placeholder="Enter Password" name="psw" required>
+                
+              <button type="submit">Login</button>
+              <label>
+                <a href="inscription.html">Inscription</a>
+              </label>
+            </div>
+
+            <div class="container" style="background-color:#f1f1f1">
+              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+              <span class="psw">Forgot <a href="#">password?</a></span>
+            </div>
+          </form>
         </div>
 
-        <div class="auth">
-          <h2>Authentification</h2>
-          
-          <label><b>Mail</b></label>
-          <input type="text" placeholder="votre Mail" name="mail" required>
+        <script>
+        // Get the modal
+        var modal = document.getElementById('id01');
 
-          <label><b>Mot de passe</b></label>
-          <input type="password" placeholder="votre mot de passe" name="pwd" required>
-              
-          <button type="submit" name="connexion">Connexion</button>
-        </div>
-      </form>
-
-      <form action="#">
-        <div class="ins" style="background-color:#f1f1f1">
-          <h2>Inscription</h2>
-
-          <label><b>Mail</b></label>
-          <input type="text" placeholder="votre mail" name="ins_mail" required>
-
-          <label><b>Mot de passe</b></label>
-          <input type="password" placeholder="votre mot de passe" name="ins_pwd" required>
-
-          <label><b>Confirmer</b></label>
-          <input type="password" placeholder="votre mot de passe" name="ins_pwd_confirm" required>
-          
-          <label><b>Prénom</b></label>
-          <input type="text" placeholder="votre prénom" name="prenom" required>
-          
-          <label><b>Nom</b></label>
-          <input type="text" placeholder="votre nom" name="nom" required>
-              
-          <button type="submit" name="inscription">Inscription</button>
-        </div>
-      </form>
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        </script>
+      
 
     <?php
     }
